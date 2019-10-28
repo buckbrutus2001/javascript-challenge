8be85827017f8c26998ceb151ad011835aaaa2df
Junghyun Son <shawnson512@gmail.com>
2019-10-27 22:53:41 -0500

    update

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,34 @@
 // from data.js
-var tableData = data;
 
-// YOUR CODE HERE!
+let ufoData = data;
+let tBody = d3.select('tbody');
+
+
+function handleSearch() {
+
+    d3.event.preventDefault();
+    let dateInput = d3.select("#datetime").node().value;
+    populatedata(dateInput);
+
+}
+
+function populatedata(){
+
+    tBody.html("");
+    let dateSearch = d3.select("#datetime").node().value;
+    let searchResult = ufoData.filter( ufo => ufo.datetime ===dateSearch);
+
+    searchResult.forEach(function(results){
+        // 	console.log(ufo);
+            const row = tBody.append('tr');
+            for (key in results){
+                const cell = tBody.append('td');
+                cell.text(results[key]);
+            }	
+        }
+    );
+}
+
+
+d3.select('#filter-btn').on('click',handleSearch);
+
